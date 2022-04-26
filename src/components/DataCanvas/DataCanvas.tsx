cc22c8d2f9da443dc5a3445cade161a4efaae947
kmjnnhyk <rlawlsgur620@gmail.com>
2022-04-26 23:11:22 +0900

    feat: API 데이터를 렌더링 할 여러개의 구체를 띄우는 작업 완료

--- src/components/DataCanvas/DataCanvas.tsx
+++ src/components/DataCanvas/DataCanvas.tsx
@@ -1,16 +1,80 @@
-import { FunctionComponent } from 'react';
+import { FunctionComponent, useRef, useState } from 'react';
 import { RInfo } from '@/types';
-
 import styles from './DataCanvas.module.scss';
+
+import * as THREE from 'three';
+import { Canvas, useFrame } from '@react-three/fiber';
+import { Instance, Instances, OrbitControls } from '@react-three/drei';
+import { MathUtils } from 'three';
 
 type DataCanvasProps = {
   data: RInfo;
 };
 
-const DataCanvas: FunctionComponent<DataCanvasProps> = function ({ data }) {
-  console.log(data);
+type ParticleType = {
+  pos: { x: number; y: number; z: number };
+  xRot: number;
+  yRot: number;
+};
 
-  return <div className={styles.container}>3D DATA CANVAS BG</div>;
+const NUMBER_OF_PARTICLE = 13;
+const POSITION_RANGE = 8;
+const ROTATION_SPEED_RANGE = 0.01;
+
+const particles: ParticleType[] = Array.from({ length: NUMBER_OF_PARTICLE }, () => ({
+  pos: {
+    x: MathUtils.randInt(-POSITION_RANGE, POSITION_RANGE),
+    y: MathUtils.randInt(-POSITION_RANGE, POSITION_RANGE),
+    z: MathUtils.randInt(-POSITION_RANGE, POSITION_RANGE),
+  },
+  xRot: MathUtils.randFloat(0.005, ROTATION_SPEED_RANGE),
+  yRot: MathUtils.randFloat(0.005, ROTATION_SPEED_RANGE),
+}));
+
+function Sphere({ pos, xRot, yRot }: ParticleType) {
+  const ref = useRef<THREE.Mesh>(null);
+  const [hovered, setHovered] = useState(false);
+
+  useFrame(() => {
+    if (ref.current != null) {
+      ref.current.position.set(pos.x, pos.y, pos.z);
+      if (!hovered) {
+        ref.current.rotation.x += xRot;
+        ref.current.rotation.y += yRot;
+      }
+    }
+  });
+
+  return <Instance ref={ref} onPointerOver={() => setHovered(true)} onPointerOut={() => setHovered(false)} />;
+}
+
+function Spheres() {
+  const ref = useRef(null);
+
+  return (
+    <Instances ref={ref} limit={NUMBER_OF_PARTICLE}>
+      <sphereBufferGeometry args={[1, 12, 12]} />
+      <meshStandardMaterial color={'hotpink'} />
+      {particles.map((data, i) => (
+        <Sphere key={i} {...data} />
+      ))}
+    </Instances>
+  );
+}
+
+const DataCanvas: FunctionComponent<DataCanvasProps> = function ({ data }) {
+  console.log('data in DataCanvas Compo', data);
+
+  return (
+    <div className={styles.container}>
+      <Canvas>
+        <ambientLight />
+        <pointLight position={[10, 10, 10]} />
+        <Spheres />
+        <OrbitControls />
+      </Canvas>
+    </div>
+  );
 };
 
 export default DataCanvas;
